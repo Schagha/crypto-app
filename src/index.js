2a150bb9440da3cb612561aa239c719e0029c8d3
Shaghay <102187856+Shaghay@users.noreply.github.com>
2023-03-28 18:15:26 +0200

    crypto news and details

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import App from './App';
 import store from './app/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-
 root.render(
   <React.StrictMode>
     <BrowserRouter>
@@ -17,53 +16,3 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
-
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <Router>
-//       <Provider store={store}>
-//         <App />
-//       </Provider>
-//     </Router>
-//   </React.StrictMode>,
-//   document.getElementById('root'),
-// );
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <Router>
-//       <App />
-//     </Router>
-//   </React.StrictMode>
-
-
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import { createBrowserRouter, RouterProvider } from "react-router-dom";
-// import Root from "./routes/root";
-
-// import App from './App';
-// import "./index.css";
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Root />,
-//   },
-//   {
-//     path: "/",
-//     element: <App />
-//   },
-// ]);
-
-// ReactDOM.createRoot(document.getElementById("root")).render([
-//   <React.StrictMode>
-//     <RouterProvider router={router} />
-//   </React.StrictMode>]
-// );
-
-
-
-
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -7,15 +7,25 @@ const cryptoApiHeaders = {
 
 const baseUrl = 'https://coinranking1.p.rapidapi.com';
 
-const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
 export const cryptoApi = createApi({
   reducerPath: 'cryptoApi',
   baseQuery: fetchBaseQuery({ baseUrl }),
   endpoints: (builder) => ({
     getCryptos: builder.query({
-      query: () => createRequest('/coins')
-    })
+      query: (count) => createRequest(`/coins?limit=${count}`)
+    }),
+    getCryptoDetails: builder.query({
+        query: (coinId) => createRequest(`/coin/${coinId}`)
+    }),
+    getCryptoHistory: builder.query({
+      query: ({coinId, timePeriod}) => createRequest(`/coin/${coinId}/history?timeperiod=${timePeriod}`)
+  }),
+  // getExchanges: builder.query({
+  //   query: () => createRequest('/exchanges'),
+  // }),
   })
 });
 
-export const { useGetCryptosQuery, } = cryptoApi;
+export const { useGetCryptosQuery, useGetCryptoDetailsQuery, useGetCryptoHistoryQuery} = cryptoApi;
