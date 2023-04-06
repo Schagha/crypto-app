d966e3ef22a274cd05c8fd4c40f4bd6be824251e
Shaghay <102187856+Shaghay@users.noreply.github.com>
2023-04-06 14:19:52 +0200

    changes in LineChart and Homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -19,11 +19,12 @@ const Homepage = () => {
     <React.Fragment>
     <Title level={2} className='heading'>Global Crypto Stats</Title>
     <Row>
-      <Col span={12}><Statistic title="Total Cryptocurrencies" value={millify(globalStats.total)} /></Col>
+      <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
       <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} /></Col>
-      <Col span={12}><Statistic title="Total Market Cap" value={millify(globalStats.totalMarketCap)} /></Col>
-      <Col span={12}><Statistic title="Total 24th Volume" value={millify(globalStats.total24thVolume)} /></Col>
-      <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarket)} /></Col>
+      <Col span={12}><Statistic title="Total Market Cap" value={`$${millify(globalStats.totalMarketCap)}`} /></Col>
+      <Col span={12}><Statistic title="Total 24h Volume" value={`$${millify(globalStats.total24hVolume)}`} /></Col>
+      <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
+      <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} /></Col>
     </Row>
     <div className='home-heading-container'>
       <Title level={2} className='home-title'>Top 10 Cryptocurrencies in the world</Title>
